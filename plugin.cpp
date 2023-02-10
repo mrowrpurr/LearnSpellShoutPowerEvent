b2fe632486ae7a5350e98988c4241ddce21ec84d
Mrowr Purr <mrowr.purr@gmail.com>
2023-02-10 12:27:53 -0800

    Kaboom

--- plugin.cpp
+++ plugin.cpp
@@ -1,12 +1,22 @@
 #include <SkyrimScripting/Plugin.h>
 
-OnInit { logger::info("Hello log from SKSE Starter Kit!"); }
-OnDataLoaded { ConsoleLog("Hello console from SKSE Starter Kit!"); }
+struct WhatDidILearn : public RE::BSTEventSink<RE::SpellsLearned::Event> {
+    RE::BSEventNotifyControl ProcessEvent(const RE::SpellsLearned::Event* event,
+                                          RE::BSTEventSource<RE::SpellsLearned::Event>*) {
+        logger::info("Learned something?");
+        if (!event) {
+            logger::info("Huh. The event, itself, was a nullptr. Huh. Weird.");
+            return RE::BSEventNotifyControl::kContinue;
+        }
+        if (!event->spell) {
+            logger::info("Huh. The spell is nullptr. Weird.");
+            return RE::BSEventNotifyControl::kContinue;
+        }
+        logger::info("Form ID learned: ", event->spell->GetFormID());
+        logger::info("Editor ID learned: ", event->spell->GetFormEditorID());
+        logger::info("Name of learned: ", event->spell->GetFullName());
+        return RE::BSEventNotifyControl::kContinue;
+    }
+};
 
-EventHandlers {
-    On<RE::TESActivateEvent>([](const RE::TESActivateEvent* event) {
-        auto activated = event->objectActivated->GetBaseObject()->GetName();
-        auto activator = event->actionRef->GetBaseObject()->GetName();
-        ConsoleLog("{} activated {}", activator, activated);
-    });
-}
+OnInit { RE::SpellsLearned::GetEventSource()->AddEventSink(new WhatDidILearn()); }
